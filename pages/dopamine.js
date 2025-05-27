8b9b09ff59b3358e20b234f904ac0a960f317091
hexagon5sides <therobloxiangaminglord@gmail.com>
2025-05-27 13:09:42 +1000

    Add files via upload

---
+++ pages/dopamine.js
@@ -0,0 +1,14 @@
+clicks = 0
+
+
+function clickFunc() {
+    clicksLabel = document.getElementById("Clicks")
+   
+    clicks +=1
+
+    clicksLabel.innerHTML = "Clicks: " + clicks
+}
+
+function UpgradeButton() {
+    
+}
